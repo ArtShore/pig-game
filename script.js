b57d437410ee6d93ec8fc0c2b312e2d26f28c033
Hossein Shouri <mr.h.shouri@gmail.com>
2024-04-08 11:50:44 +0330

    Grabbed elements from DOM I grabbed all the elements that i needed from DOM and stored them inside variables for ease of use

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const pZeroOverallScore = document.querySelector('#score--0');
+const pOneOverallScore = document.querySelector('#score--1');
+const pZeroCurrentScore = document.querySelector('#current--0');
+const pOneCurrentScore = document.querySelector('#current--1');
+const newGameBtn = document.querySelector('.btn--new');
+const rollDiceBtn = document.querySelector('.btn--roll');
+const holdScoreBtn = document.querySelector('.btn--hold');
+const dice = document.querySelector('.dice');
