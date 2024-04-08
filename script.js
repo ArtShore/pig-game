1da8c719213a840790d56d914dc20c6f42d7ecf0
Hossein Shouri <mr.h.shouri@gmail.com>
2024-04-08 12:07:19 +0330

    grabbed a couple more elements

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const pZeroOverallScore = document.querySelector('#score--0');
-const pOneOverallScore = document.querySelector('#score--1');
-const pZeroCurrentScore = document.querySelector('#current--0');
-const pOneCurrentScore = document.querySelector('#current--1');
+const playerZeroSect = document.querySelector('.player--0');
+const playerOneSect = document.querySelector('.player--1');
+const pZeroOverallScore = document.querySelector('#score--0').textContent;
+const pOneOverallScore = document.querySelector('#score--1').textContent;
+const pZeroCurrentScore = document.querySelector('#current--0').textContent;
+const pOneCurrentScore = document.querySelector('#current--1').textContent;
 const newGameBtn = document.querySelector('.btn--new');
 const rollDiceBtn = document.querySelector('.btn--roll');
 const holdScoreBtn = document.querySelector('.btn--hold');
-const dice = document.querySelector('.dice');
+const theDice = document.querySelector('.dice');
